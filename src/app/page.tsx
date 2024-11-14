d6cfdfc56c5b6870b2181b9352929d1b0bf9725e
Ayush Singh <ayush1009208@gmail.com>
2024-11-15 02:46:23 +0530

    Signup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,47 @@ import { div } from "framer-motion/client";
 import Heading from "./components/heading";
 import { AppleCardsCarouselDemo } from "./components/applecarddemo";
 import { FeaturesSectionDemo } from "./components/middle/background-boxes";
+import dynamic from 'next/dynamic'
+
 export default function LampDemo() {
   return (
     <div className="class">
       <Navbar/>
-    <LampContainer>
+      <LampContainer>
      
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Build lamps <br /> the right way
-      </motion.h1>
-    </LampContainer>
+
+  <motion.h1
+    initial={{ opacity: 0.5, y: 100 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}
+    className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+  >
+    Build lamps <br /> the right way
+  </motion.h1>
+
+
+</LampContainer>
+<h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        Get to know your iSad.
+      </h2>
+      <motion.button
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{
+    duration: 1,
+    ease: "easeInOut",
+  }}
+  className="px-10 py-3 mt-12 pl-4 ml-36 mx-auto max-w-7xl text-xl md:text-5xl font-bold bg-blue-500 text-neutral-800 dark:text-neutral-200 font-sans rounded-lg hover:bg-blue-600 transition-colors"
+  onClick={() => alert("Button Clicked!")}
+>
+  Click Me
+</motion.button>
+
+
     <AppleCardsCarouselDemo/>
     
     <FeaturesSectionDemo/>
@@ -43,7 +65,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden ",
         className
       )}
     >
